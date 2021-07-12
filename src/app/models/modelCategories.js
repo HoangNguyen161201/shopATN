dba783b510cf0bef7d83b24cc297b91fafe42e7a
HoangNguyen161201 <84957563+HoangNguyen161201@users.noreply.github.com>
2021-07-12 11:40:05 +0700

    main

--- src/app/models/modelCategories.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoose_delete = require('mongoose-delete');
-// Define the fields and the type of each field
-const category = new mongoose.Schema({
-    _id: Number,
-    name: String
-},{id:false});
-category.plugin(mongoose_delete);
-module.exports = mongoose.model('category',category);
